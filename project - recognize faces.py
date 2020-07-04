da6a8cb73ef298587c5537f95345d478b60c6f1e
Aviad Bitton <aviadbi1@gmail.com>
2020-07-04 19:44:27 +0300

    recognize faces in pictures and video

--- project - recognize faces.py
+++ project - recognize faces.py
@@ -1,27 +1,96 @@
 import cv2
+import numpy as np
 import face_recognition
+import os
 
-imgLior = face_recognition.load_image_file('faces/Lior.jpg')
-imgLior = cv2.cvtColor(imgLior, cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('faces/JamesCorden.jpg')
-imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
-faceLoc = face_recognition.face_locations(imgLior)[0]
-encodeLior = face_recognition.face_encodings(imgLior)[0]
-cv2.rectangle(imgLior, (faceLoc[3], faceLoc[0]),
-              (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+def readFacesFromDir(dirPath):
+    images = []
+    classNames = []
+    myList = os.listdir(dirPath)
+    # print(myList)
+    for faceClass in myList:
+        curImg = cv2.imread(f'{dirPath}/{faceClass}')
+        images.append(curImg)
+        classNames.append(os.path.splitext(faceClass)[0])
+    print(classNames)
+    return images, classNames
 
-faceLocTest = face_recognition.face_locations(imgTest)[0]
-encodeTest = face_recognition.face_encodings(imgTest)[0]
-cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]),
-              (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
 
-results = face_recognition.compare_faces([encodeLior], encodeTest)
-faceDis = face_recognition.face_distance([encodeLior], encodeTest)
-print(results, faceDis)
-cv2.putText(imgTest, f'{results} {round(faceDis[0],2)}', (
-    50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+def findEncodingsOfAFace(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
 
-cv2.imshow('Lior Suchard', imgLior)
-cv2.imshow('Lior?', imgTest)
-cv2.waitKey(0)
+
+def findFaceLocationInImage(img, imgName):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(img)
+    for faceLoc in facesCurFrame:
+        y1, x2, y2, x1 = faceLoc
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        # cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+
+    cv2.imshow(imgName, img)
+
+
+def identifyImage(img, imgName):
+    scale = 2
+    smallerImg = cv2.resize(img, (0, 0), None, 1.0/scale, 1.0/scale)
+    smallerImg = cv2.cvtColor(smallerImg, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(smallerImg)
+    encodesCurFrame = face_recognition.face_encodings(
+        smallerImg, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDistance = face_recognition.face_distance(
+            encodeListKnown, encodeFace)
+        print(faceDistance)
+        matchIndex = np.argmin(faceDistance)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*scale, x2*scale, y2*scale, x1*scale
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1+6, y2-6),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.imshow(imgName, img)
+
+
+dirPath = 'faces'
+testDirPath = 'facesTest'
+
+images, classNames = readFacesFromDir(dirPath)
+encodeListKnown = findEncodingsOfAFace(images)
+print('Encoding Complete')
+
+
+testList = os.listdir(testDirPath)
+for testFileName in testList:
+    imgName = os.path.splitext(testFileName)[0]
+    if testFileName.endswith("jpg"):
+        curImg = cv2.imread(f'{testDirPath}/{testFileName}')
+        identifyImage(curImg, imgName)
+        # findFaceLocationInImage(curImg, imgName)
+
+        cv2.waitKey(0)
+    if testFileName.endswith('mp4'):
+        cap = cv2.VideoCapture(f'{testDirPath}/{testFileName}')
+        # cap = cv2.VideoCapture(0)  # videocam
+
+        while cap.isOpened():
+            success, img = cap.read()
+            identifyImage(img, imgName)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break  # press Q for quit
